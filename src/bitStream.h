f02d839d07cbb75c2a6565f8cf3d071e4bce21ba
Igor Timofeev <methodman1@mail.ru>
2025-12-26 01:28:50 +0300

    чота медленновато пока

--- src/bitStream.h
+++ src/bitStream.h
@@ -12,7 +12,7 @@ namespace YOBA {
 			{
 				setBitIndex(bitIndex);
 			}
-
+			
 			inline uint8_t* getBuffer() const {
 				return _buffer;
 			}
@@ -20,44 +20,44 @@ namespace YOBA {
 			inline size_t getBitIndex() const {
 				return _bitIndex;
 			}
-
+			
 			void setBitIndex(size_t value) {
 				_bitIndex = value;
-
+				
 				updateByteIndicesFromBitIndex();
 			}
-
+			
 			inline size_t getByteIndex() const {
 				return _byteIndex;
 			}
-
+			
 			size_t getBytesProcessed() const {
 				return _byteBitIndex == 0 ? _byteIndex : _byteIndex + 1;
 			}
-
+			
 			bool readBool() {
-				const auto result = (*(_buffer + _byteIndex) >> _byteBitIndex) & 0b1;
+				const auto result = (_buffer[_byteIndex] >> _byteBitIndex) & 0b1;
 				nextBit();
-
+				
 				return result;
 			}
-
+			
 			uint8_t readUint8(uint8_t bits = 8) {
 				return readUnsigned<uint8_t>(bits);
 			}
-
+			
 			uint16_t readUint16(uint8_t bits = 16) {
 				return readUnsigned<uint16_t>(bits);
 			}
-
+			
 			int16_t readInt16(uint8_t bits = 16) {
-				return readSigned<int16_t>(bits);
+				return readSigned<int16_t, uint16_t>(bits);
 			}
-
+			
 			int32_t readInt32(uint8_t bits = 32) {
-				return readSigned<int32_t>(bits);
+				return readSigned<int32_t, uint32_t>(bits);
 			}
-
+			
 			uint32_t readUint32(uint8_t bits = 32) {
 				return readUnsigned<uint32_t>(bits);
 			}
@@ -75,7 +75,7 @@ namespace YOBA {
 			}
 			
 			void writeInt16(int16_t value, uint8_t bits = 16) {
-				writeSigned<int16_t>(value, bits);
+				writeSigned<int16_t, uint16_t>(value, bits);
 			}
 			
 			void writeUint32(uint32_t value, uint8_t bits = 32) {
@@ -83,7 +83,7 @@ namespace YOBA {
 			}
 			
 			void writeInt32(int32_t value, uint8_t bits = 32) {
-				writeSigned<int32_t>(value, bits);
+				writeSigned<int32_t, uint32_t>(value, bits);
 			}
 			
 			void writeFloat(float value, uint8_t bits = 32) {
@@ -102,56 +102,56 @@ namespace YOBA {
 				
 				nextByte();
 			}
-
+		
 		private:
 			uint8_t* _buffer;
-
+			
 			size_t _bitIndex = 0;
 			size_t _byteIndex = 0;
 			uint8_t _byteBitIndex = 0;
-
+			
 			void updateByteIndicesFromBitIndex() {
 				_byteIndex = _bitIndex / 8;
 				_byteBitIndex = _bitIndex % 8;
 			}
-
+			
 			void nextBit() {
 				_bitIndex++;
 				_byteBitIndex++;
-
+				
 				if (_byteBitIndex >= 8) {
 					_byteIndex++;
 					_byteBitIndex = 0;
 				}
 			}
-
+			
 			template<typename TNumber>
 			TNumber readUnsigned(uint8_t bits) {
 				TNumber result = 0;
-
+				
 				for (int i = 0; i < bits; ++i) {
-					result |= (((*(_buffer + _byteIndex) >> _byteBitIndex) & 0b1) << i);
+					result |= (((_buffer[_byteIndex] >> _byteBitIndex) & 0b1) << i);
 					nextBit();
 				}
-
+				
 				return result;
 			}
-
-			template<typename TNumber>
-			TNumber readSigned(uint8_t bits) {
+			
+			template<std::signed_integral TSigned, std::unsigned_integral TUnsigned>
+			TSigned readSigned(uint8_t bits) {
 				// Magnitude
-				TNumber magnitude = 0;
-
+				TUnsigned magnitude = 0;
+				
 				for (int i = 0; i < bits - 1; ++i) {
-					magnitude |= (((*(_buffer + _byteIndex) >> _byteBitIndex) & 0b1) << i);
+					magnitude |= (((_buffer[_byteIndex] >> _byteBitIndex) & 0b1) << i);
 					nextBit();
 				}
-
+				
 				// Sign
-				const auto sign = ((*(_buffer + _byteIndex) >> _byteBitIndex) & 0b1) == 1;
+				const auto sign = ((_buffer[_byteIndex] >> _byteBitIndex) & 0b1) == 1;
 				nextBit();
-
-				return sign ? -magnitude : magnitude;
+				
+				return sign ? -static_cast<TSigned>(magnitude) : static_cast<TSigned>(magnitude);
 			}
 			
 			void writeBit(bool value) {
@@ -176,11 +176,13 @@ namespace YOBA {
 				}
 			}
 			
-			template<std::signed_integral TNumber>
-			void writeSigned(TNumber value, uint8_t bits) {
+			template<std::signed_integral TSigned, std::unsigned_integral TUnsigned>
+			void writeSigned(TSigned value, uint8_t bits) {
+				const auto unsignedValue = static_cast<TUnsigned>(std::abs(value));
+				
 				// Magnitude
 				for (uint8_t i = 0; i < bits - 1; ++i) {
-					writeBit(((value >> i) & 0b1) == 1);
+					writeBit(((unsignedValue >> i) & 0b1) == 1);
 				}
 				
 				// Sign
